4aafbe97e900a8326d9362bbcd9270825f8159d5
PedroMiguelAlmeida <pedromiguel.almd@gmail.com>
2023-03-25 16:54:29 +0000

    sentDataHistory array structure to store messages log

--- index.js
+++ index.js
@@ -2,16 +2,27 @@
 const http = require('http');
 const fs = require('fs');
 const WebSocket = require('ws');
+const { receiveMessageOnPort } = require('worker_threads');
 
 
 const PORT = 8080;
+
 const wss = new WebSocket.Server({port:3002});
 
+let sentDataHistory = new Array();
 
-wss.on('connection', function connection(ws) {
-    console.log("Client connected")
+wss.on('connection', function connection(ws){
+    //console.log(`Length before sending a new message ${sentDataHistory.length}`);
     ws.on('message', function incoming(data) {
+      sentDataHistory.push(data);
+      //console.log(`Data history: ${sentDataHistory.toString()}`);
+      //console.log(`Length after sending a new message ${sentDataHistory.length}`);
+      if (sentDataHistory.length == 1) {
+        //console.log("Esta é a primeira mensagem");
+        // function to create schema based on this message;
+      }
       wss.clients.forEach(function each(client) {
+        //console.log(wss.clients) //Shows SET data structure which wss uses to store every ws
         if (client != ws && client.readyState === WebSocket.OPEN) {
           client.send(data.toString());
           console.log(`Data sent by client: ${data}`)
@@ -29,16 +40,3 @@ fs.readFile('./index.html',function(error,html){
     response.end();
   }).listen(PORT);
 });
-
-
-// wss.on('connection',function connection(ws) {
-//   console.log("Client connected");
-//   ws.on('message',data => {
-//     console.log(`Client has send ${data}`);
-//     if(ws.readyState == WebSocket.OPEN){
-//       ws.send(data);
-//     }
-//   })
-  
-// })
-
