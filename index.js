4bccb4fc958c59bcc86854732f49fa98136920a9
Tiago <clptuga@gmail.com>
2023-03-27 15:58:04 +0100

    first commit wip

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 
-const http = require('http');
-const fs = require('fs');
-const WebSocket = require('ws');
+const http = require('http')
+const fs = require('fs')
+const WebSocket = require('ws')
 
 
 const PORT = 8080;
-const wss = new WebSocket.Server({port:3002});
+const wss = new WebSocket.Server({port:3002})
+
+var rooms = []
+var id = 0
 
 
-wss.on('connection', function connection(ws) {
-    console.log("Client connected")
-    ws.on('message', function incoming(data) {
+wss.on('connection', function connection(ws, req) {
+  id++
+  ws.id = id
+
+  console.log(wss.id)
+
+  ws.on('open', () => {  console.log("Connection opened") })
+
+  
+  ws.on('message', (data) => {
+    if(data.room){
+      if(!rooms[data.room].includes(ws))
+        rooms[data.room].push(ws)
+
+      console.log(rooms)
       wss.clients.forEach(function each(client) {
         if (client != ws && client.readyState === WebSocket.OPEN) {
           client.send(data.toString());
           console.log(`Data sent by client: ${data}`)
         }
       })
-    })
+    }
   })
+
+    ws.on('close', () => { 
+      console.log("Connection closed") 
+
+      rooms.forEach( (room) => {
+        room = myArray.filter(function( obj ) {
+          return obj.field !== 'money';
+        });
+      });
+      })
+  })
+
 
 //mounting http server for index.html
 fs.readFile('./index.html',function(error,html){
@@ -31,14 +58,4 @@ fs.readFile('./index.html',function(error,html){
 });
 
 
-// wss.on('connection',function connection(ws) {
-//   console.log("Client connected");
-//   ws.on('message',data => {
-//     console.log(`Client has send ${data}`);
-//     if(ws.readyState == WebSocket.OPEN){
-//       ws.send(data);
-//     }
-//   })
-  
-// })
 
