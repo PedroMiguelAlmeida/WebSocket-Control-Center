10b2d97fc8c7092742563d3cce681248ef9b8bb3
Tiago <clptuga@gmail.com>
2023-04-01 22:35:21 +0100

    first simple rooms complete: add room, add client, remove client from all rooms on disconnect

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const WebSocket = require('ws')
 const PORT = 8080;
 const wss = new WebSocket.Server({port:3002})
 
-var rooms = []
+var rooms = [{room : "room1"}]
 var id = 0
 
 
@@ -15,35 +15,62 @@ wss.on('connection', function connection(ws, req) {
   id++
   ws.id = id
 
-  console.log(wss.id)
+  console.log("id - "+ws.id)
 
   ws.on('open', () => {  console.log("Connection opened") })
 
   
-  ws.on('message', (data) => {
-    if(data.room){
-      if(!rooms[data.room].includes(ws))
-        rooms[data.room].push(ws)
+  ws.on('message', (data)  => {
+    let dataObj = null
 
-      console.log(rooms)
-      wss.clients.forEach(function each(client) {
+    try{
+      dataObj = JSON.parse(data);
+    
+      if(!dataObj.room){
+        ws.send("Error - room not set!");
+        throw "Error - room not set!"
+      }
+
+      var room = rooms.find(obj => { return obj.room === dataObj.room })
+      //console.log("room - " + JSON.stringify(room));
+      if(room){
+        room.clients = room.clients || []
+        var client = room.clients.find(obj => { return obj === ws })
+        if(!client)
+          room.clients.push(ws)
+      }else{
+        rooms.push({ room : dataObj.room, clients : [ws] })
+        room = { room : dataObj.room, clients : [ws] }
+      }
+      //console.debug("rooms - " + JSON.stringify(rooms));
+
+      room.clients.forEach(function each(client) {
         if (client != ws && client.readyState === WebSocket.OPEN) {
-          client.send(data.toString());
-          console.log(`Data sent by client: ${data}`)
+          client.send(dataObj.data);
+          console.log("Data sent to client:"+dataObj.data);
+        }
+        if (client == ws && client.readyState === WebSocket.OPEN) {
+          client.send("Data sent to clients successfully!");
         }
       })
     }
+    catch(error){
+      console.error(error);
+    }
+    
   })
 
-    ws.on('close', () => { 
-      console.log("Connection closed") 
+    ws.on('close', () => {
+      console.log("Connection closed - Client "+ws.id) 
 
-      rooms.forEach( (room) => {
-        room = myArray.filter(function( obj ) {
-          return obj.field !== 'money';
+      rooms.forEach((room) => {
+        room.clients = room.clients.filter(function( obj ) {
+            return obj.id !== ws.id;
         });
       });
-      })
+
+      console.log("Client "+ws.id+" removed from all rooms");
+    })
   })
 
 
